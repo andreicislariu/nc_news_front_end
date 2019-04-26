c21e96a545b3f9f47d374a037e22d8aba0166e9d
Andrei Florian <andreicislariu@gmail.com>
2019-04-26 14:54:08 +0100

    add getUser and vote fetching

--- src/api.js
+++ src/api.js
@@ -11,18 +11,29 @@ export const getArticles = async queries => {
   const { data } = await axios.get(
     `${URL}/articles${queries ? `?${queries.join('&')}` : ''}`
   );
-  console.log(data.articles);
   return data.articles;
 };
 
 export const getArticlesById = async id => {
   const { data } = await axios.get(`${URL}/articles/${id}`);
-  console.log(data.article);
+  console.log(data.article, '<--- data.article');
   return data.article;
 };
 
 export const getCommentsByArticleId = async article_id => {
   const { data } = await axios.get(`${URL}/articles/${article_id}/comments`);
-  console.log(data);
+  console.log(data, '<--- data (comments)');
   return data;
+};
+
+export const getUser = async username => {
+  const { data } = await axios.get(`${URL}/users/${username}`);
+  console.log(data, '<--- data username');
+  return data;
+};
+
+export const vote = async (inc_votes, id) => {
+  const { data } = await axios.patch(`${URL}/articles/${id}`, { inc_votes });
+  console.log(data.article, '<---- data.article for votes');
+  return data.article;
 };
