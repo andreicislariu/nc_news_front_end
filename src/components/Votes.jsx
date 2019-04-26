d8ccb13b9e18d5b42231b83002471bfcb047a78a
Andrei Florian <andreicislariu@gmail.com>
2019-04-26 14:33:31 +0100

    add Votes component

---
+++ src/components/Votes.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import * as api from '../api';
+
+class Votes extends Component {
+  state = {
+    changeVote: 0
+  };
+  render() {
+    const { changeVote } = this.state;
+    const { votes } = this.props;
+    return (
+      <div>
+        <button onClick={() => this.vote(1)} disabled={changeVote === 1}>
+          {' '}
+          <span role="img" aria-label="Up-Arrow" aria-labelledby="uparrow1">
+            ⬆️
+          </span>
+        </button>
+        <p>Votes: {votes}</p>
+        <button onClick={() => this.vote(-1)} disabled={changeVote === -1}>
+          <span role="img" aria-label="Down-Arrow" aria-labelledby="downarrow1">
+            ⬇️
+          </span>
+        </button>
+      </div>
+    );
+  }
+
+  vote = inc_votes => {
+    const { id, section } = this.props;
+    api.vote(inc_votes, id, section).catch(err => {
+      this.setState(state => ({
+        changeVote: state.changeVote - inc_votes
+      }));
+    });
+
+    this.setState(state => ({
+      changeVote: state.changeVote + inc_votes
+    }));
+  };
+}
+
+export default Votes;
