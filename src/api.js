75a3334b2fa7c501fe4407d8329100a3b5fed1f9
Andrei Florian <andreicislariu@gmail.com>
2019-04-26 09:25:25 +0100

    add api.js for fetching api

--- src/api.js
+++ src/api.js
@@ -4,7 +4,6 @@ const URL = 'https://nc-news-ap-i.herokuapp.com/api';
 
 export const getTopics = async () => {
   const { data } = await axios.get(`${URL}/topics`);
-  console.log(data.topics);
   return data.topics;
 };
 
@@ -20,4 +19,10 @@ export const getArticlesById = async id => {
   const { data } = await axios.get(`${URL}/articles/${id}`);
   console.log(data.article);
   return data.article;
+};
+
+export const getCommentsByArticleId = async article_id => {
+  const { data } = await axios.get(`${URL}/articles/${article_id}/comments`);
+  console.log(data);
+  return data;
 };
