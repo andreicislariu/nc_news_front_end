6b3ac55da3a4d737f8afc40e0a47ca026ab2e6e4
Andrei Florian <andreicislariu@gmail.com>
2019-04-29 16:43:50 +0100

    modify class names for button

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -11,15 +11,33 @@ class Votes extends Component {
 
     return (
       <div>
-        <button onClick={() => this.vote(1)} disabled={changeVote === 1}>
+        <button
+          className="voteArrowButton"
+          onClick={() => this.vote(1)}
+          disabled={changeVote === 1}
+        >
           {' '}
-          <span role="img" aria-label="Up-Arrow" aria-labelledby="uparrow1">
+          <span
+            className="spanArrow"
+            role="img"
+            aria-label="Up-Arrow"
+            aria-labelledby="uparrow1"
+          >
             ⬆️
           </span>
         </button>
         <p>Votes: {votes + changeVote}</p>
-        <button onClick={() => this.vote(-1)} disabled={changeVote === -1}>
-          <span role="img" aria-label="Down-Arrow" aria-labelledby="downarrow1">
+        <button
+          className="voteArrowButton"
+          onClick={() => this.vote(-1)}
+          disabled={changeVote === -1}
+        >
+          <span
+            className="spanArrow"
+            role="img"
+            aria-label="Down-Arrow"
+            aria-labelledby="downarrow1"
+          >
             ⬇️
           </span>
         </button>
@@ -29,7 +47,6 @@ class Votes extends Component {
 
   vote = inc_votes => {
     const { id, section } = this.props;
-    console.log(id, 'votes article id');
     api.vote(inc_votes, id, section).catch(err => {
       this.setState(state => ({
         changeVote: state.changeVote - inc_votes
