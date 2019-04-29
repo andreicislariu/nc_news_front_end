ab800e87c7e630f851b9de0eb3684cd076f23131
Andrei Florian <andreicislariu@gmail.com>
2019-04-29 13:05:56 +0100

    add functionality to logout button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Link } from '@reach/router';
+import '../css/App.css';
 
-const Header = () => {
+const Header = props => {
   return (
     <main className="header">
       <h1>
+        {/* <Link></Link> */}
         <span role="img" aria-label="Newspaper" aria-labelledby="newspaper">
           📰 NC News
         </span>
       </h1>
+      <Link to="/" onClick={props.handleLogOut}>
+        <button className="logout">Log Out</button>
+      </Link>
     </main>
   );
 };
